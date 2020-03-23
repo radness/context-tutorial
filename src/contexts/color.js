5de11b852ea278e7fe7105c1aeb2f5b073a9203b
radness <yhs5128@gmail.com>
2020-03-23 23:53:59 +0900

    Context file 수정하기

--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,5 +1,33 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-const ColorContext = createContext({ color: "black" });
+const ColorContext = createContext({
+  state: {
+    color: "black",
+    subcolor: "red"
+  },
+  actions: {
+    setColor: () => {},
+    setSubcolor: () => {}
+  }
+});
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState("black");
+  const [subcolor, setSubcolor] = useState("red");
+
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubcolor }
+  };
+  return (
+    <ColorContext.Proivder value={value}>{children}</ColorContext.Proivder>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer 와 같은 의미
+const { Consumer: ColorConsumer } = ColorContext;
+
+// ColorProvider와 ColorComsuner 내보내기
+export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
